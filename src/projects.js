0b21289b9d2ed24241a8818d3a02597f96291ed0
ninasmrekar <nina.smrekar@ost.ch>
2024-11-26 17:48:23 +0100

    add newsletter project

--- src/projects.js
+++ src/projects.js
@@ -1,118 +1,125 @@
 const projects = [
     {
         id: 1,
+        title: "Newsletter",
+        description: "Subscription to a newsletter.",
+        image: "newsletter.PNG",
+        link: "https://ninasmrekar.github.io/test-newsletter/"
+    },
+    {
+        id: 2,
         title: "Landing Page",
         description: "Landing Page for landscape photography made with HTML and CSS.",
         image: "landing-page.PNG",
         link: "https://ninasmrekar.github.io/landing-page/"
     },
     {
-        id: 2,
+        id: 3,
         title: "Food Order App",
         description: "Web application for ordering food. This is only a test site and no orders can actually be placed!",
         image: "food-order-app.PNG",
         link: "https://food-order-app-nina.netlify.app"
     },
     {
-        id: 3,
+        id: 4,
         title: "Tic Tac Toe",
         description: "Tic Tac Toe game made with React.",
         image: "tic-tac-toe.PNG",
         link: "https://tic-tac-toe-nina.netlify.app/"
     },
     {
-        id: 4,
+        id: 5,
         title: "Investment Calculator",
         description: "Investment Calculator game made with React.",
         image: "investment-calculator.PNG",
         link: "https://investment-calculator-nina.netlify.app"
     },
     {
-        id: 5,
+        id: 6,
         title: "Random Overload Blog",
         description: "Blog where users can create and read posts. Made with HTML, CSS, JavaScript and Node.js. Web application loads very slowly!",
         image: "random-overload-blog.PNG",
         link: "https://random-overload-blog.onrender.com"
     },
     {
-        id: 6,
+        id: 7,
         title: "Poetry Website",
         description: "Web application for searching poetry. Made with HTML, CSS, Bootstrap, Javascript and Node.js. Web application loads very slowly!",
         image: "lost-in-poetry.PNG",
         link: "https://lost-in-poetry-website.onrender.com/"
     },
     {
-        id: 7,
+        id: 8,
         title: "TO DO List",
         description: "Web application for creating daily, weekly, monthly and yearly to do lists. Made with HTML, CSS, Javascript, Node.js, PostgreSQL and Passport.js Authentication.",
         image: "to-do-list.PNG",
         link: "https://github.com/ninasmrekar/to-do-list"
     },
     {
-        id: 8,
+        id: 9,
         title: "Mondrian",
         description: "Mondrian painting made with HTML, CSS and CSS Grid.",
         image: "mondrian.PNG",
         link: "https://ninasmrekar.github.io/mondrian/"
     },
     {
-        id: 9,
+        id: 10,
         title: "Pricing Table",
         description: "Pricing table made with HTML, CSS and CSS Flexbox.",
         image: "pricing-table.PNG",
         link: "https://ninasmrekar.github.io/pricing-table/"
     },
     {
-        id: 10,
+        id: 11,
         title: "Quote Poster",
         description: "Motivational poster made with HTML and CSS.",
         image: "motivational-poster.PNG",
         link: "https://ninasmrekar.github.io/motivational-poster/"
     },
     {
-        id: 11,
+        id: 12,
         title: "Hotel Project",
         description: "Web Design for a hotel using Canva.",
         image: "hotel-project.PNG",
         link: "https://hotel-project-nina.my.canva.site"
     },
     {
-        id: 12,
+        id: 13,
         title: "Winery Project",
         description: "Web Design for a winery using Canva.",
         image: "winery.PNG",
         link: "https://hotel-project-nina.my.canva.site/winery"
     },
     {
-        id: 13,
+        id: 14,
         title: "Simon Game",
         description: "Simon Game made with HTML, CSS and Javascript.",
         image: "simon-game.PNG",
         link: "https://ninasmrekar.github.io/simon-game/"
     },
     {
-        id: 14,
+        id: 15,
         title: "Drum Kit",
         description: "Drum Kit made with HTML, CSS and Javascript.",
         image: "drum-kit.PNG",
         link: "https://ninasmrekar.github.io/drum-kit/"
     },
     {
-        id: 15,
+        id: 16,
         title: "Dice Game",
         description: "Dice Game made with HTML, CSS and Javascript.",
         image: "dice-game.PNG",
         link: "https://ninasmrekar.github.io/dice-game/"
     },
     {
-        id: 16,
+        id: 17,
         title: "Project Management",
         description: "Project management app made with React.",
         image: "project-management.PNG",
         link: "https://project-management-nina.netlify.app/"
     },
     {
-        id: 17,
+        id: 18,
         title: "Rock Paper Scissors",
         description: "Rock Paper Scissors made with HTML, CSS, Javascript and APIs. This game is only in german! Web application loads very slowly!",
         image: "rock-paper-scissors.PNG",
